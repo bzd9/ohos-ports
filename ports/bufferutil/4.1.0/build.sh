33523d9f9cdd5a4c99c107882c3019914e964da0
zhenghongfei <hongfeizheng@163.com>
2026-08-03 20:18:32 +0800

    add @ohos-ports/bufferutil@4.1.0

---
+++ ports/bufferutil/4.1.0/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+# 准备源码
+curl -fsSL https://github.com/websockets/bufferutil/archive/refs/tags/v4.1.0.tar.gz -o bufferutil-4.1.0.tar.gz
+tar -zxf bufferutil-4.1.0.tar.gz
+rm bufferutil-4.1.0.tar.gz
+cd bufferutil-4.1.0
+patch -p1 < ../patchs/0001-update-package-json.patch
+
+# 构建 addon
+npm install
+npm run prebuild
+
+# 把其他平台的预构建产物复制到包里面一起发布
+cd ..
+curl -fsSL https://registry.npmjs.org/bufferutil/-/bufferutil-4.1.0.tgz -o bufferutil-4.1.0.tgz
+tar -zxf bufferutil-4.1.0.tgz
+rm bufferutil-4.1.0.tgz
+
+cp -r package/prebuilds/* bufferutil-4.1.0/prebuilds/
+rm -rf package
+
+cd bufferutil-4.1.0/prebuilds
+mv openharmony-arm64/bufferutil.node openharmony-arm64/ohos-ports+bufferutil.node
+mv darwin-arm64/bufferutil.node darwin-arm64/ohos-ports+bufferutil.node
+mv darwin-x64/bufferutil.node darwin-x64/ohos-ports+bufferutil.node
+mv linux-x64/bufferutil.node linux-x64/ohos-ports+bufferutil.node
+mv win32-ia32/bufferutil.node win32-ia32/ohos-ports+bufferutil.node
+mv win32-x64/bufferutil.node win32-x64/ohos-ports+bufferutil.node
